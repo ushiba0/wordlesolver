1a6de535280ee49273d2aed39e1d617cfd9cbb95
u.shiba <lychnus372@gmail.com>
2022-02-23 09:49:45 +0900

    Add files via upload

--- wordle.js
+++ wordle.js
@@ -20,12 +20,9 @@ const onKeyDown = e => {
             // Move cursor backward
             if(cursor.word===0 && cursor.char===0){
                 // Nothing to do
-
-                console.warn("koko1")
             }else if(cursor.char === 0){
                 cursor.char = 4;
                 cursor.word -= 1;
-                console.warn("koko2")
             }else{
                 cursor.char -= 1;
             }
